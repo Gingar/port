72be5d777592c395283edda042839c95360ea918
Alex Krapivin <alex.krapivin@lge.com>
2012-08-31 20:11:31 +0400

    Build script "build.sh" modified to * compile modules in BUILD dir * dowload and unpack Debian packages * install packages and modules to DEPLOY NOTE: probably one STAGE dir would be enough

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 DEPOT="`dirname \"$0\"`" 
-DEPOT="`( cd \"$DEPOT\" && cd .. && pwd )`"  # absolutized and normalized
+DEPOT="`( cd \"$DEPOT\" && pwd )`"  # absolutized and normalized
 if [ -z "$DEPOT" ] ; then
   # error; for some reason, the path is not accessible
   # to the script (e.g. permissions re-evaled after suid)
@@ -17,40 +17,87 @@ PC="-DCMAKE_TOOLCHAIN_FILE=$DEPOT/config/Platform_PC.cmake"
 
 PLATFORM=PC
 if [ "$1" != "" ]; then
-	TOOL=$1
+  TOOL=$1
 fi
 
 if [ "$2" != "" ]; then
-	PLATFORM=$2
+  PLATFORM=$2
 fi
 
 eval TOOLCHAIN=\$$PLATFORM
-#echo Toolchain: $TOOLCHAIN
+echo "Toolchain: $TOOLCHAIN"
+
+function addPackage()
+{
+  DEB_PAKAGES="${DEB_PAKAGES} ${1}${DEB_ARCH}.deb"
+}
+
+DEB_FTP=http://ftp.us.debian.org/debian/pool/main/
+DEB_ARCH=i386
+DEB_PAKAGES=
+addPackage b/boost1.42/libboost1.42-dev_1.42.0-4_
+addPackage b/boost1.42/libboost-thread1.42.0_1.42.0-4_
+addPackage b/boost1.42/libboost-thread1.42-dev_1.42.0-4_
+addPackage b/boost1.42/libboost-filesystem1.42.0_1.42.0-4_
+addPackage b/boost1.42/libboost-filesystem1.42-dev_1.42.0-4_
+addPackage b/boost1.42/libboost-system1.42.0_1.42.0-4_
+addPackage b/boost1.42/libboost-system1.42-dev_1.42.0-4_
+addPackage b/boost1.42/libboost-regex1.42.0_1.42.0-4_
+addPackage b/boost1.42/libboost-regex1.42-dev_1.42.0-4_
+addPackage g/gtest/libgtest0_1.5.0-1_
+addPackage g/gtest/libgtest-dev_1.5.0-1_
+
+export PREFIX=$DEPOT/DEPLOY/usr
+mkdir -p $PREFIX
+cd $DEPOT/DEPLOY
+#local debpackage
+for deburl in ${DEB_PAKAGES}; do
+  deburl=${DEB_FTP}${deburl}
+  echo "Downloading $deburl..."
+  wget -nc -nv ${deburl}
+  if [ $? -ne 0  ]
+    then exit 1
+  fi
+done
+
+for debfile in *; do
+  debtemplate="\.deb$"
+  if [[ ${debfile} =~ ${debtemplate} ]]; then
+    echo "Unpacking ${debfile}..."
+    dpkg -x ${debfile} .
+    if [ $? -ne 0  ]
+      then exit 1
+    fi
+  fi
+done
 
 function buildModule()
 {
-	prefix=$1
-	module=$2
-	echo Building $module...
-	mkdir $module
-	cd $module
-	cmake $TOOLCHAIN $DEPOT/$prefix/$module 
-	if [ $? -ne 0  ]
-		then exit 1
-	fi 
-	make $MAKE_OPTS install 
-	if [ $? -ne 0  ] 
-		then exit 1
-	fi
-	cd ..
+  module=$1
+  echo "====================================================================="
+  echo "Building $module..."
+  echo " "
+  mkdir -p $DEPOT/BUILD/$module
+  cd $DEPOT/BUILD/$module
+  cmake $TOOLCHAIN $DEPOT/$module
+  if [ $? -ne 0  ]
+    then exit 1
+  fi
+
+echo "MAKE_OPTS=$MAKE_OPTS"
+  make $MAKE_OPTS install
+  if [ $? -ne 0  ]
+    then exit 1
+  fi
+  cd $DEPOT
 }
 
 MODULES=build.conf
-cat $DEPOT/tool/$TOOL/$MODULES | while read lib;
-	do buildModule "lib" $lib
+cat $DEPOT/ginga/$MODULES | while read lib;
+  do buildModule $lib
 done
 if [ $? -ne 0  ] 
-	then exit 1
+  then exit 1
 fi
-buildModule "tool" $TOOL
+buildModule ginga
 
